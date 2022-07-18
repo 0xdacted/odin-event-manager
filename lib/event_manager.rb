10a8f06431d2ffe53785c9824b1c9348e98948a2
Red <crabmarket56@gmail.com>
2022-07-18 15:25:55 -0400

    Iterate over incorrect/absent zipcode entries

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,5 @@
 require 'csv'
+
 puts 'EventManager initialized.'
 
 contents = CSV.open(
@@ -10,5 +11,12 @@ contents = CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
+
+  if zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  end
+
   puts "#{name} #{zipcode}"
 end
