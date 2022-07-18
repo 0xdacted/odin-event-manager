f3fd9e5fdba3e64278309c49e17322bfe7927e30
Red <crabmarket56@gmail.com>
2022-07-18 18:17:46 -0400

    Convert legislators output to string to clean output

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -20,16 +20,21 @@ contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
-begin
-  legislators = civic_info.representative_info_by_address(
-    address: zipcode,
-    levels: 'country',
-    roles: ['legislatorUpperBody', 'legislatorLowerBody']
-  )
-  legislators = legislators.officials
-  legislator_names = legislators.map(&:name)
-rescue
-  'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-end
-  puts "#{name} #{zipcode} #{legislator_names}"
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+
+    legislator_names = legislators.map(&:name)
+
+    legislators_string = legislator_names.join(", ")
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+
+  puts "#{name} #{zipcode} #{legislators_string}"
 end
