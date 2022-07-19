02796666a1636084e59dd83e2d0e712c4666dc7d
Red <crabmarket56@gmail.com>
2022-07-19 15:40:36 -0400

    Add phone number cleaning method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,23 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def clean_home_phone(home_phone)
+  home_phone = home_phone.to_s.tr('-', '').tr(' ', '').tr('(', '').tr(')', '').tr('.', '')
+  case
+  when home_phone.length < 10 || home_phone.length > 11
+    puts home_phone = '0000000000'
+  
+  when home_phone.length == 10
+    puts home_phone
+
+  when home_phone.length == 11 && home_phone[0] == 1
+    puts home_phone = home_phone[1..10]
+
+  when home_phone.length == 11 && home_phone[0] != 1
+    puts home_phone = '0000000000'
+  end
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -46,6 +63,7 @@ contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  home_phone = clean_home_phone(row[:homephone])
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
